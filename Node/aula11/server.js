5067ab75caa72b88b924f1620244884611bd5d43
Airton <airtondeodato2007@hotmail.com>
2023-07-06 20:25:42 -0300

    estudos de pastas completas front-back

---
+++ Node/aula11/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path')
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+app.use(routes);
+
+// // /profiles/12345 ?
+// //campanha=googleads &
+// //nome_campanha=seila
+
+// app.get('/', (req, res) => {
+//     res.send(`<form action"/" method="POST">
+//     Nome do cliente: <input type="text" name="nome">
+//     <button>Enviar Form</button>
+//     `);
+// });
+
+// app.get('/testes/:idUsuarios?', (req, res) => { //interrogação faz ser opcional! e para adicionar parametros usamos /:
+//     // /profile/3
+//     // /profiles/?chave1=valor&chave2=valor2&chave3=valor3
+//     //
+//     console.log(req.params);
+//     console.log(req.query);
+//     res.send(req.params.idUsuarios);
+// })
+
+// app.post('/', (req, res) =>{ //o metodo post está do form method post.
+//     console.log(req.body)
+//     res.send(`O que você me enviou foi: ${req.body.nome}`);//esse nome vem do name do forms
+// });
+
+
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+} );
---
+++ Node/aula13/src/middlewares/middeware.js
@@ -0,0 +1,10 @@
+exports.middlewareGlobal = (req, res, next) => { 
+    next();
+}
+// module.exports = (req, res, next) => { 
+    //     next();
+    // }
+    exports.outroMiddleware = (req, res, next) => { 
+        console.log('Sou seu outro middleware');
+        next();
+    }
