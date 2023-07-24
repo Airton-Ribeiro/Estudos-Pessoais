e53f3a41574ecd809589a4db8b4b75cc39d8eef2
Airton <airtondeodato2007@hotmail.com>
2023-07-24 14:55:13 -0300

    conexão banco

--- Node/aula14/server.js
+++ Node/aula14/server.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-const connectionString = ''
-mongoose.connect()
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    app.emit('pronto');
+  })
+  .catch(e=> console.log(e));
 
 const routes = require("./routes");
 const path = require("path");
@@ -21,7 +25,11 @@ app.use(middlewareGlobal);
 
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Acessar http://localhost:3000");
-  console.log("Servidor executando na porta 3000");
+app.on('pronto', () => {
+  app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
 });
+
+
